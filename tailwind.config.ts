6d7ecdf6e89121bef9e6657f6a2fc1d7a6556a9c
Q <quentin.fogarty@catchdesign.co.nz>
2023-11-23 13:17:58 +1300

    basic font styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,17 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    theme: {
+      fontSize: {
+        sm: "0.8rem",
+        base: "1rem",
+        xl: "1.25rem",
+        "2xl": "1.563rem",
+        "3xl": "1.953rem",
+        "4xl": "2.441rem",
+        "5xl": "3.052rem",
+      },
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
